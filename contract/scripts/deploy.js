735c570eb3e48801e596c56e5ad9a087f5bfe89c
Zahid Nawaz <151016187+ShahidNawazBlockchain@users.noreply.github.com>
2024-03-08 11:45:22 -0800

    finally complete setup the hardhat project project

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -4,23 +4,20 @@ async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+   const lockedAmount = hre.ethers.utils.parseEther("0.001");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-  await lock.deployed()
+  const NFT_MarketPlace = await hre.ethers.deployContract("NFT_MarketPlace");
+  await NFT_MarketPlace.deployed()
 
   console.log(
-    `Lock with ${hre.ethers.utils.formatEther(
+    `NFT MarketPlace with ${hre.ethers.utils.formatEther(
       lockedAmount
-    )} ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    )} ETH and  timestamp ${unlockTime} deployed to ${NFT_MarketPlace.address}`
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
