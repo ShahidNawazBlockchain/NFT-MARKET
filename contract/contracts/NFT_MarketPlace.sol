62b89f47b085d6ab0e5b268069625b4a51bdde11
Zahid Nawaz <151016187+ShahidNawazBlockchain@users.noreply.github.com>
2024-03-08 18:18:38 -0800

    change openziplin version

--- contract/contracts/NFT_MarketPlace.sol
+++ contract/contracts/NFT_MarketPlace.sol
@@ -1,5 +1,12 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.4;
-contract NFT_MarketPlace {
-  
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
+contract NFT_MarketPlace is ERC721URIStorage {
+   using Counters for Counters.Counter;
+
+   Counters.Counter private _tokenIds;
+   Counters.Counter private _itemsSold;
 }
