35508421bde6ab2d1db2807bf30dbca3d5f3c9d9
Zahid Nawaz <151016187+ShahidNawazBlockchain@users.noreply.github.com>
2024-03-12 02:25:29 -0700

    just deploy and verify smart contract and also verify the smart contract on sepolia testnet

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,10 +1,3 @@
-// require("@nomicfoundation/hardhat-toolbox");
-
-// /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: "0.8.24",
-// };
-
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomicfoundation/hardhat-verify")
@@ -12,14 +5,11 @@ require("@nomicfoundation/hardhat-verify")
 const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
-
-  
   solidity: "0.8.4",
   defaultNetwork: "sepolia",
   sourcify: {
     enabled: false
   },
-  
   networks: {
     hardhat: {},
     sepolia: {
